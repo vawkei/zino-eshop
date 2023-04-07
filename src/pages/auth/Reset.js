100f5f5df2f1c9dd856af4f43cb2855a8f91141f
voke sokoh <vokes9810@gmail.com>
2023-04-07 18:15:22 +0800

    Worked on the Resetjs reset password logic

--- src/pages/auth/Reset.js
+++ src/pages/auth/Reset.js
@@ -2,11 +2,36 @@ import classes from "./auth.module.scss";
 import resetImg from "../../assets/eshopforgot.png";
 import { Link } from "react-router-dom";
 import Card from "../../components/ui/Card";
-
-
-
+import { useState } from "react";
+import {  sendPasswordResetEmail } from "firebase/auth";
+import { toast } from "react-toastify";
+import { auth } from "../../firebase/config";
+import Loader from "../../components/loader/Loader";
 
 const Reset = () => {
+  const [email, setEmail] = useState("");
+  const [isLoading,setIsLoading] =useState(false);
+  
+  const emailChangeHandler = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const submitHandler= (event)=>{
+    event.preventDefault();
+    setIsLoading(true)
+
+    sendPasswordResetEmail(auth, email)
+  .then(() => {
+    
+    toast('Check your Mail for Reset Link')
+    setIsLoading(false)
+  })
+  .catch((error) => {
+    toast(error.message)
+    setIsLoading(false)
+  });
+  }
+
   return (
     <div>
       <section className={`${classes.auth} section`}>
@@ -18,20 +43,25 @@ const Reset = () => {
           <div className={classes.form}>
             <h2>Reset Password</h2>
 
-            <form action="">
+            {isLoading && <Loader/>}
+            <form action="" onSubmit={submitHandler}>
+              <input
+                type="text"
+                placeholder="Password"
+                value={email}
+                required
+                onChange={emailChangeHandler}
+              />
 
-              <input type="text" placeholder="Password" required />
-
-              <button className="--btn --btn-primary --btn-block">Reset Password</button>
+              <button className="--btn --btn-primary --btn-block" type="submit">
+                Reset Password
+              </button>
 
               <div className={classes.links}>
                 <Link to={"/login"}>- Login</Link>
                 <Link to={"/register"}>- Register</Link>
               </div>
             </form>
-
-
-            
           </div>
         </Card>
       </section>
